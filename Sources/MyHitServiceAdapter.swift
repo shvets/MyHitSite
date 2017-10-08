af5f2aa3dcde16e0104380e1fe5386b464cefc5d
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-08 13:27:30 -0400

    relax requirements to items

--- Sources/MyHitServiceAdapter.swift
+++ Sources/MyHitServiceAdapter.swift
@@ -128,15 +128,15 @@ class MyHitServiceAdapter: ServiceAdapter {
     }
   }
 
-  override func addBookmark(item: MediaItem) -> Bool {
+  override func addBookmark(item: Item) -> Bool {
     return bookmarks.addBookmark(item: item)
   }
 
-  override func removeBookmark(item: MediaItem) -> Bool {
+  override func removeBookmark(item: Item) -> Bool {
     return bookmarks.removeBookmark(item: item)
   }
 
-  override func addHistoryItem(_ item: MediaItem) {
+  override func addHistoryItem(_ item: Item) {
     history.add(item: item)
   }
 
