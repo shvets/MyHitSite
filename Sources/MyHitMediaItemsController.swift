2172b816e6789d310a0d388d7c64aebd05a406e6
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-24 22:17:03 -0400

    code improvement

---
+++ Sources/MyHitMediaItemsController.swift
@@ -0,0 +1,104 @@
+import TVSetKit
+import AudioPlayer
+
+open class MyHitMediaItemsController: MediaItemsController {
+  override open func navigate(from view: UICollectionViewCell, playImmediately: Bool=false) {
+    if let indexPath = collectionView?.indexPath(for: view),
+       let mediaItem = items.getItem(for: indexPath) as? MediaItem {
+
+      if mediaItem.isContainer() {
+        if mediaItem.isAudioContainer() {
+          if mediaItem.hasMultipleVersions() {
+            performSegue(withIdentifier: AudioVersionsController.SegueIdentifier, sender: view)
+          }
+          else {
+            performSegue(withIdentifier: AudioItemsController.SegueIdentifier, sender: view)
+          }
+        }
+        else {
+          if let destination = MediaItemsController.instantiateController(configuration?["storyboardId"] as! String) {
+           destination.configuration = configuration
+
+           for (key, value) in self.params {
+             destination.params[key] = value
+           }
+
+           destination.params["selectedItem"] = mediaItem
+           destination.params["parentId"] = mediaItem.id
+           destination.params["parentName"] = mediaItem.name
+           destination.params["isContainer"] = true
+
+           if !mobile {
+             if let layout = configuration?["buildLayout"] {
+               destination.collectionView?.collectionViewLayout = layout as! UICollectionViewLayout
+             }
+
+             present(destination, animated: true)
+           }
+           else {
+             navigationController?.pushViewController(destination, animated: true)
+           }
+         }
+        }
+      }
+      else {
+        super.navigate(from: view, playImmediately: playImmediately)
+      }
+    }
+  }
+
+  // MARK: Navigation
+
+  override open func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let identifier = segue.identifier,
+       let selectedCell = sender as? MediaItemCell {
+
+      if let indexPath = collectionView?.indexPath(for: selectedCell) {
+        let mediaItem = items[indexPath.row] as! MediaItem
+
+        switch identifier {
+        case AudioItemsController.SegueIdentifier:
+          if let destination = segue.destination as? AudioItemsController {
+            destination.name = mediaItem.name
+            destination.thumb = mediaItem.thumb
+            destination.id = mediaItem.id
+
+            if let requestType = params["requestType"] as? String {
+              if requestType != "History" {
+                historyManager?.addHistoryItem(mediaItem)
+              }
+            }
+
+            destination.pageLoader.load = {
+              var items: [AudioItem] = []
+
+              var newParams = Parameters()
+
+              for (key, value) in self.params {
+                newParams[key] = value
+              }
+
+              newParams["requestType"] = "Tracks"
+              newParams["selectedItem"] = mediaItem
+              newParams["convert"] = false
+
+              if let data = try self.dataSource?.load(params: newParams) {
+                if let mediaItems = data as? [[String: String]] {
+                  for mediaItem in mediaItems {
+                    items.append(AudioItem(name: mediaItem["name"]!, id: mediaItem["id"]!))
+                  }
+                }
+              }
+
+              return items
+            }
+          }
+
+        default:
+          super.prepare(for: segue, sender: sender)
+        }
+      }
+    }
+  }
+
+}
